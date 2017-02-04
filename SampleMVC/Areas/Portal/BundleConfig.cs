95ad842edbafc3b0c82a9e51e86ebfc965084734
Kevin McNamee <kevin.mcnamee1@gmail.com>
2017-02-04 18:54:33 +0000

    Commit initial .NET MVC project

---
+++ SampleMVC/Areas/Portal/BundleConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Optimization;
+
+namespace SampleMVC.Portal.Controllers
+{
+    internal static class BundleConfig
+    {
+        internal static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/Sample/css").Include(
+                          "~/Areas/Portal/Content/SampleStyleSheet.css"));
+        }
+    }
+}
---
+++ SampleMVC/Areas/Portal/PortalAreaRegistration.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Mvc;
+using System.Web.Optimization;
+
+namespace SampleMVC.Areas.Portal
+{
+    public class PortalAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Portal";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            string[] nameSpaces = { "SampleMVC.Portal.Controllers" };
+            context.MapRoute(
+                "Portal",
+                "Portal/{controller}/{action}/{id}",
+                new {controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: nameSpaces
+            );
+            RegisterBundles();
+        }
+        private void RegisterBundles()
+        {
+            SampleMVC.Portal.Controllers.BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
